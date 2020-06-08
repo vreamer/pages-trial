5e0fe61c19931c811e573fe00ebf13b28c2d3813
Vreamer <yhou.vreamer@gmail.com>
2020-06-08 10:39:41 +0800

    Updates

---
+++ precache-manifest.05a36a25c1eded0884c5dde781c84b68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b5ce83cd4b765b319a26bc768cfebc",
+    "url": "/pages-trial/index.html"
+  },
+  {
+    "revision": "a45eadc3e76f23f632b3",
+    "url": "/pages-trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "271586bc986065c4264d",
+    "url": "/pages-trial/static/js/2.bec614eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages-trial/static/js/2.bec614eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45eadc3e76f23f632b3",
+    "url": "/pages-trial/static/js/main.5f68481b.chunk.js"
+  },
+  {
+    "revision": "365e1dc80e03d24c69a8",
+    "url": "/pages-trial/static/js/runtime-main.828c4b2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pages-trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
